c885b07fb23d4e3dedc205267770c7595790360c
Eric Baker <elb5465@gmail.com>
2022-01-06 10:48:48 -0500

    Update amazonCodingTest.py

--- amazonCodingTest.py
+++ amazonCodingTest.py
@@ -20,18 +20,53 @@ print(cnt)
 
 
 # ------------------ QUESTION 2: --------------------
-# Number of ways to select 3 pages such that no two adjacent selected pages are of the same type (ordinary or bookmarked)
-# Required: Permutations
+''' Number of ways to select 3 pages such that no two adjacent selected pages are of the same type (ordinary or bookmarked) '''
 
-''' notes:
-        - find all permutations of 3 digits 
-        - filter out only the ones that are have alternating binary (101 or 010)
-        - count all that are left including duplicates
-        - OR
-        - (sliding window / pointer mxethod)
-        - ...
-'''
+def number_of_ways(b):
+	l = len(b)
+	i = -1
+	j = -1
+	cnt = 0
+	temp = ""
+	k = 0
 
-# def number_of_ways(s):
-            
+	while (i != l-2):
+		if (i == -1):
+			i = k
+			k += 1
+			# print(i,j,k)
+			continue
 
+		if (j == -1):
+			j = k
+			k += 1
+			# print(i,j,k)
+			continue
+
+		if (i != -1) and (j != -1):
+			temp = b[i]+b[j]+b[k]
+			# print(temp)
+
+			if (temp=="010" or temp=="101"):
+				cnt += 1
+
+			temp = ""
+
+		if (k==l-1):
+			if (j!=l-2):
+				j += 1
+				k = j+1
+			else:
+				i += 1
+				j = i+1
+				k = j+1
+		else:
+			k += 1
+
+		#print(i,j,k)
+	
+	return cnt
+			
+print("expected: 3, actual: ", number_of_ways("10111"))
+print("expected: 4, actual: ", number_of_ways("01001"))
+print("expected: 0, actual: ", number_of_ways("0001"))

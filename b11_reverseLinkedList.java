71a69b12bdd78d2568311493fc65ffddb813233a
Eric Baker <elb5465@gmail.com>
2022-01-08 22:28:07 -0500

    Update b11_reverseLinkedList.java

--- b11_reverseLinkedList.java
+++ b11_reverseLinkedList.java
@@ -10,14 +10,18 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
+        // need to save a prev var to point to while reversing
         ListNode prev = null;
-
+        
+        // save next node first, so you can adjust current pointer without losing the next node
         while (head != null){
             ListNode next = head.next;
             head.next = prev;
             prev = head;
             head = next;
         }
+        
+        // returning previous bc loop breaks when head = null, therefore last valid node is the previous 
         return prev;
     }
 }

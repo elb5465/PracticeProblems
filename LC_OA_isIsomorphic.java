b021d3db9779f80608228c5aacc4b428f7f9540b
Eric Baker <elb5465@gmail.com>
2022-02-04 14:48:55 -0500

    Create LC_OA_isIsomorphic.java

---
+++ LC_OA_isIsomorphic.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int sl = s.length();
+        int tl = t.length();
+        
+        if (sl!=tl) return false;
+        
+        // get cnt of all characters for each in 2 separate hashmaps
+        
+        // iterate over both hashmaps and add their values to 2 stacks
+        
+        // pop the items off the stacks and compare until no values left
+            // if any don't match, return false
+        
+        // otherwise return true
+    }
+}

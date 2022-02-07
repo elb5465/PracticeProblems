0981d431e0a1ebdadeec2b5cc9039c31d933b255
Eric Baker <elb5465@gmail.com>
2022-02-06 22:30:42 -0500

    Create LC_OC_longestCommonPrefix.java

---
+++ LC_OC_longestCommonPrefix.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String cp = "";  // common prefix
+        int l = strs.length();
+        
+        // get minimum length of the strings
+
+        // iterate over, check curr, add char to result if all strings contain the char at the same places
+    }
+}
